862ebaef438ded94c3f7f809918c406fcc1b848e
MainMachine <ozanozy75@gmail.com>
2017-12-03 13:57:30 +0300

    *laravel is empty *voyager is empty *voyager have'nt admin user

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Ozan Özyıldırım Site
                 </div>
 
                 <div class="links">
